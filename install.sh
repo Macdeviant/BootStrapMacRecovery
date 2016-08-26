e7c3b5d09cd8b3936c1cca1b76c6e40bdb1b6940
Craig Hair <craig.hair@gmail.com>
2016-08-26 12:45:07 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+diskutil partitionDisk /dev/disk0 2 GPT JHFS+ "Macintosh HD" 100% 
+echo "Disk has been re-partitioned."
+
+
+#local asr
+asr restore -source Desktop/ModularSystem.dmg -target
+/Volumes/Macintosh\ HD/ -erase -noprompt -noverify
+
+#network asr
+asr restore -source http://www.pretendco.com/deployment/
+ModularSystem.dmg -target /Volumes/Macintosh\ HD/ -erase -noprompt -noverify
+
+
+# Apply Restore image with asr
+echo "Restore the Restore with ASR"
+/usr/sbin/asr restore -source
+/Volumes/Data/Packages/Restore_Image_Leopard.dmg -target /Volumes/Restore
+-erase -noprompt -noverify
